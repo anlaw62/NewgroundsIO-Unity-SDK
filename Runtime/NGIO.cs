7c40ad588042119f6488efcbd2d79aac753be5a5
anlaw62 <anlaw62@mail.ru>
2025-03-26 20:59:00 +0300

    [add] appid,aeskey, gatewayuri

--- Runtime/NGIO.cs
+++ Runtime/NGIO.cs
@@ -1,8 +1,19 @@
+using Cysharp.Threading.Tasks;
+using System.Threading.Tasks;
 using UnityEngine;
+
 namespace Newgrounds
 {
     public class NGIO
     {
-
+        public string AppId { get; private set; }
+        public string AesKey { get; private set; }
+        internal const string GATEWAY_URI = "https://www.newgrounds.io/gateway_v3.php";
+        public NGIO(string appId, string aesKey)
+        {
+            AppId = appId;
+            AesKey = aesKey;
+        }
+      
     }
 }
