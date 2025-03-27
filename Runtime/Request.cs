16497716a2e50c87a22d9220e919596795d2cf4d
anlaw62 <anlaw62@mail.ru>
2025-03-27 18:01:32 +0300

    [add] encryption

--- Runtime/Request.cs
+++ Runtime/Request.cs
@@ -5,6 +5,10 @@ using Unity.Serialization.Json;
 
 using Unity.Properties;
 using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+using System;
 namespace Newgrounds
 {
     [GeneratePropertyBag]
@@ -41,6 +45,7 @@ namespace Newgrounds
         [GeneratePropertyBag]
         internal partial class ExecuteObject
         {
+
             public string Echo
             {
                 get => echo;
@@ -62,6 +67,30 @@ namespace Newgrounds
             private string echo;
             [CreateProperty]
             protected string component;
+            [CreateProperty]
+            private string secure;
+            public void Encrypt(byte[] key)
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    aes.Key = key;
+                    aes.GenerateIV();
+
+                    using (var encryptor = aes.CreateEncryptor())
+                    using (var ms = new MemoryStream())
+                    using (var cryptoStream = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        byte[] input = Encoding.UTF8.GetBytes(JsonSerialization.ToJson(this));
+                        cryptoStream.Write(input, 0, input.Length);
+                        cryptoStream.FlushFinalBlock();
+                        secure = Convert.ToBase64String(ms.ToArray());
+                    }
+                    echo = default;
+                    component = default;
+                    parameters = default;
+
+                }
+            }
         }
 
     }
